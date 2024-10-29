22ed85c310489996b34976e8dcfe6644c3da70f9
umamahesh <vumamaheshwararaov@gmail.com>
2024-10-29 12:45:20 +0530

    docker files

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "sg_id" {
+  type        = string
+  default     = "sg-0e78201b3e0335a6d"
+  description = "description"
+}
+
+variable "public_subnet_id" {
+    type = string
+    default = "subnet-06fe2626378723cdd"
+}
